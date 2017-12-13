d9e2b5d4f051b01072501afd644eeab56ad49191
Clément Bauchet <clement.bauchet44@gmail.com>
2017-12-13 16:46:27 +0100

    POST

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -2,6 +2,7 @@ package dev.sgp.web;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -35,27 +36,42 @@ public class EditerCollaborateurController extends HttpServlet {
 		String nomParam = req.getParameter("nom");
 		String prenomParam = req.getParameter("prenom");
 		
+		//NB : utiliser x-www-form-urlencoded
+		
+		//Vérification des paramètres
+		ArrayList<String> missingParameters = new ArrayList<>();
+		if (matriculeParam == null || matriculeParam.equals("")) {
+			missingParameters.add("matricule");
+		}
+		if (titreParam == null || titreParam.equals("")) {
+			missingParameters.add("titre");
+		}
+		if (nomParam == null || nomParam.equals("")) {
+			missingParameters.add("nom");
+		}
+		if (prenomParam == null || prenomParam.equals("")) {
+			missingParameters.add("prenom");
+		}
+		
 		resp.setContentType("text/html");
 		// code HTML ecrit dans le corps de la reponse
 		
-//		
-//		BufferedReader reader = req.getReader();
-//		String line;
-//		do {
-//			
-//		} while (line != null);
-		
-		
-		
-		resp.getWriter().write("<h1>Édition de collaborateur</h1>");
-		if (matriculeParam == null || matriculeParam.equals("")) {
-			resp.getWriter().write("<p>Erreur : un matricule est attendu</p>");
+		if (missingParameters.size() > 0) {
+			StringBuilder message = new StringBuilder("Les paramètres suivants sont incorrects : ");
+			for (String p : missingParameters) {
+				message.append(p).append(" ");
+			}
+			resp.sendError(400, message.toString());
 		} else {
+			resp.getWriter().write("<h1>Édition de collaborateur</h1>");
 			resp.getWriter().write("<p>Matricule="+ matriculeParam + "</p>");
 			resp.getWriter().write("<p>Titre="+ titreParam + "</p>");
 			resp.getWriter().write("<p>Nom="+ nomParam + "</p>");
-			resp.getWriter().write("<p>Prenom="+ prenomParam + "</p>");
+			resp.getWriter().write("<p>Prenom="+ prenomParam + "</p>");			
 		}
 		
+		
 	}
+	
+	
 }
